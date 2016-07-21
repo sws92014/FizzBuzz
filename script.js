5196df5bb6faffcc4c56263a7f4229559a194ff7
Sarah Smith <sws92014@gmail.com>
2016-07-20 21:10:21 -0500

    fixed script to be cleaner

--- script.js
+++ script.js
@@ -1,43 +1,40 @@
 $(document).ready(function() {
 	$('#submit').on('click', function(e) {
 		e.preventDefault();
-		var number = +$('#query').val();
+		var number = $('#query').val();
+			number = parseInt(number);
 
 		// make sure section is empty before clearing
-		if (!$('section').is(':empty')) $('section').html('');
+		if (!$('section').is(':empty')) {
+			$('section').html('');
+		}
 
 		// make sure span is empty before clearing message
-		if (!$('span').is(':empty')) $('span').html('');
+		if (!$('#error').is(':empty')) {
+			$('#error').html('');
+		}
  
-		// check if number is integer
+		// check if number is an integer
 		if (number % 1 === 0) {
+		 } else {
+			$('#error').append('<p>Input must be an integer only</p>');
+		}
 
 		// check if number is positive
-		if (number > 0) {
+		if (number <= 0) {
+			$('#error').append('<p>Input cannot be a negative value</p>');
+		}
 
-		// check if number less than 101
-		if (number < 101) {
+		// check if number is less than 101
+		if (number > 100) {
+			$('#error').append('<p>Input cannot be a number greater than 100</p>');
+		} else {
 			appendFizzBuzz(number);
-
-		} else {
-			$('span').append('</p>Input cannot be a number greater than 100</p>');
-		}
-
-		    
-		} else {
-			$('span').append('</p>Input cannot be a negative value</p>');
-		}
-
-
-		} else {
-			$('span').append('</p>Input must be an integer only</p>');
 		}
 
 	});
 });
 
-
- 
  	var appendFizzBuzz = function (n) {
 		for (var i = 1; i <= n; i++) {
 
