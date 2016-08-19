325c95cc7dc42265d7a4d23a9e9ea099913fd74b
sws92014 <sws92014@gmail.com>
2016-08-18 22:14:56 -0500

    cleaned up js and reduced the amounts of IFs

--- script.js
+++ script.js
@@ -1,64 +1,55 @@
 $(document).ready(function() {
 	$('#submit').on('click', function(e) {
 		e.preventDefault();
-		var number = +$('#query').val();
+		var number = $('#query').val();
+		number = parseInt(number);
 
 		// make sure section is empty before clearing
-		if (!$('section').is(':empty')) $('section').html('');
+		if (!$('section').is(':empty')) {
+			$('section').html('');
+		}
 
 		// make sure span is empty before clearing message
-		if (!$('span').is(':empty')) $('span').html('');
- 
-		// check if number is integer
+		if (!$('#error').is(':empty')) {
+			$('#error').html('');
+		}
+
+		// check if number is an integer
 		if (number % 1 === 0) {
+		} else {
+			$('#error').append('<p>Input must be an integer only</p>');
+		}
 
 		// check if number is positive
-		if (number > 0) {
+		if (number <= 0) {
+			$('#error').append('<p>Input cannot be a negative value</p>');
+		}
 
-		// check if number less than 101
-		if (number < 101) {
+		// check if number is less than 101
+		if (number > 100) {
+			$('#error').append('<p>Input cannot be a number greater than 100</p>');
+		} else {
 			appendFizzBuzz(number);
-
-		} else {
-			$('span').append('Input cannot be a number greater than 100!');
-		}
-
-		    
-		} else {
-			$('span').append('Input cannot be negative value!');
-		}
-
-
-		} else {
-			$('span').append('Input must be an integer.');
 		}
 
 	});
 });
 
-
- 
- 	var appendFizzBuzz = function (n) {
-		for (var i = 1; i <= n; i++) {
-
-		// Check if 'i' is divisible by both 3 and 5
-		if (i % 15 === 0) {
-			$('section').append('fizz buzz');
+function appendFizzBuzz(n) {
+	for (var i = 1; i <= n; i++) {
+		var current = "";
 
 		// Check if 'i' is divisible by 3
-		} else if (i % 3 === 0) {
-			$('section').append('fizz');
-
+		if (i % 3 === 0) {
+			current += "fizz";
+			
 		// Check if 'i' is divisible by 5
-		} else if (i % 5 === 0) {
-			$('section').append('buzz');
-
-		// If none of the above, print 'i'
-		} else {
-			$('section').append(i);
+		} 
+		if (i % 5 === 0) {
+			current += "buzz";
 		}
 
 		// Add a line break after each iteration of the for loop
-		$('section').append('<br />');
+		$('section').append((current || i)+'<br />');
 	}
 };
