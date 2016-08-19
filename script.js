fb1e30f729525a7287160ceed104541dccca1318
sws92014 <sws92014@gmail.com>
2016-08-18 22:15:11 -0500

    cleaned up js and reduced the amounts of IFs

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	$('#submit').on('click', function(e) {
 		e.preventDefault();
 		var number = $('#query').val();
-			number = parseInt(number);
+		number = parseInt(number);
 
 		// make sure section is empty before clearing
 		if (!$('section').is(':empty')) {
@@ -13,10 +13,10 @@ $(document).ready(function() {
 		if (!$('#error').is(':empty')) {
 			$('#error').html('');
 		}
- 
+
 		// check if number is an integer
 		if (number % 1 === 0) {
-		 } else {
+		} else {
 			$('#error').append('<p>Input must be an integer only</p>');
 		}
 
@@ -35,27 +35,21 @@ $(document).ready(function() {
 	});
 });
 
- 	var appendFizzBuzz = function (n) {
-		for (var i = 1; i <= n; i++) {
-
-		// Check if 'i' is divisible by both 3 and 5
-		if (i % 15 === 0) {
-			$('section').append('fizz buzz');
+function appendFizzBuzz(n) {
+	for (var i = 1; i <= n; i++) {
+		var current = "";
 
 		// Check if 'i' is divisible by 3
-		} else if (i % 3 === 0) {
-			$('section').append('fizz');
-
+		if (i % 3 === 0) {
+			current += "fizz";
+			
 		// Check if 'i' is divisible by 5
-		} else if (i % 5 === 0) {
-			$('section').append('buzz');
-
-		// If none of the above, print 'i'
-		} else {
-			$('section').append(i);
+		} 
+		if (i % 5 === 0) {
+			current += "buzz";
 		}
 
 		// Add a line break after each iteration of the for loop
-		$('section').append('<br />');
+		$('section').append((current || i)+'<br />');
 	}
 };
